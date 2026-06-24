9743f5a302e93260668ce6ccf1eae81014b14c57
Rupert Tscheliessnig <rupert.tscheliessnig@gmail.com>
2026-06-24 17:48:41 +0200

    Add missing React entry file for web build

---
+++ web/src/index.jsx
@@ -0,0 +1,15 @@
+/**
+ * React Entry Point
+ * 
+ * Mounts App component to #root
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
